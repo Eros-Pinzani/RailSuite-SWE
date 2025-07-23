19b1d57058e29117ad0feba33f51ebe7457b7792
Eros-Pinzani <eros.pinzani@edu.unifi.it>
2025-07-23 10:27:23 +0200

    Implementation of CarriageMapper and DepotMapper

--- src/dao/CarriageDepotDaoImp.java
+++ src/dao/CarriageDepotDaoImp.java
@@ -7,13 +7,7 @@ import java.util.List;
 
 class CarriageDepotDaoImp implements CarriageDepotDao {
     private static CarriageDepot mapResultSetToCarriageDepot(ResultSet rs) throws SQLException {
-        return CarriageDepot.of(
-            rs.getInt("id_depot"),
-            rs.getInt("id_carriage"),
-            rs.getTimestamp("time_entered"),
-            rs.getTimestamp("time_exited"),
-            CarriageDepot.StatusOfCarriage.valueOf(rs.getString("status_of_carriage"))
-        );
+        return mapper.CarriageDepotMapper.toDomain(rs);
     }
 
     @Override
